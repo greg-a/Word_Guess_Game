17ee6084ed76dc7ac5252ba7f2e5fd97861c22db
Greg Allebach <greg.allebach@gmail.com>
2020-04-23 12:11:27 -0400

    made function to fill in correct guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,83 @@
 // press any key to start
-// show _ for every unguessed letter in answer
-// guesses remaining
-// letters already guessed
-// show correct letters
+
+// push correct guesses to blankAnswer
 
 var wins = 0;
-var lettersGuessed = [];
+var correctGuesses = [];
+var incorrectGuesses = [];
 var answers = ["the process", "trust the process", "sam hinkie", "joel embiid", "ben simmons"];
 var guessesRemaining = 10;
 var computerAnswer = answers[Math.floor(Math.random() * answers.length)];
+var blankAnswer = "";
+var correctAnswer = [];
 
+function newAnswer() {
+    for (var i = 0; i < computerAnswer.length; i++) {
+        computerAnswer = computerAnswer.replace(/\s+/g, '');
+        correctAnswer.push(computerAnswer[i]);        
+        blankAnswer += "_ ";
+    }
+}
+
+function fillBlank() {
+    blankAnswer = "";
+
+    for (var i = 0; i < correctAnswer.length; i++) {
+        if (correctGuesses.includes(correctAnswer[i])) {
+            blankAnswer += correctAnswer[i];
+        }
+        else {
+            blankAnswer += "_ ";
+        }
+    }
+}
+
+
+ 
 
 document.onkeyup = function(event) {
 
     var userGuess = event.key.toLowerCase(); 
-    var correctAnswer = [];
+    
+    
 
     function checkGuess(letter) {
         return letter !== userGuess;
     }
 
-    for (var i = 0; i < computerAnswer.length; i++) {
-        correctAnswer.push(computerAnswer[i]);
+    
+    if (correctAnswer.length === 0) {
+        newAnswer();
+        document.getElementById("blank-answer").innerHTML = blankAnswer;
     }
 
-    if (lettersGuessed.every(checkGuess)) {
+    if (incorrectGuesses.every(checkGuess) && (correctGuesses.every(checkGuess))) {
 
         if (correctAnswer.every(checkGuess) === false) {
-            alert("Correct Guess!");
-            lettersGuessed.push(userGuess);
+            alert("Correct Guess! ");
+            correctGuesses.push(userGuess);
+            fillBlank(correctGuesses);
+            document.getElementById("blank-answer").innerHTML = blankAnswer;
         }
+        
         else {
             if (guessesRemaining > 1) {
                 guessesRemaining--;
-                lettersGuessed.push(userGuess);
+                incorrectGuesses.push(userGuess);
             }
             else {
-                lettersGuessed.length = 0;
-                guessesRemaining = 10;
-                computerAnswer = answers[Math.floor(Math.random() * answers.length)];
+                correctGuesses.length = 0;
+                incorrectGuesses.length = 0;
+                correctAnswer.length = 0;
+                blankAnswer = "";
+                guessesRemaining = 10;                
             }
         }  
     }
-    
+      
     console.log("Answer: " + computerAnswer);
-    console.log("Letters guessed: " + lettersGuessed);
+    document.getElementById("wrong-guess").innerHTML = "Wrong guesses: " + incorrectGuesses;
+    console.log("Wrong Guesses: " + incorrectGuesses);
     console.log("Correct Answer: " + correctAnswer);
     console.log("Guesses Remaining: " + guessesRemaining);
 
